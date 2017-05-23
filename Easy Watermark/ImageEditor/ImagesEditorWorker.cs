ac70b052e6fd48f51520a6a23e4ca8d61dabcb48
Adam Brzozowski <adam_prohack@hotmail.com>
2017-05-23 03:08:07 +0200

    Added ImageEditorWorker class

---
+++ Easy Watermark/ImageEditor/ImagesEditorWorker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace EasyWatermark
+{
+    public class ImagesEditorWorker
+    {
+        private static Queue<string> ImagesPaths = new Queue<string>();
+        public static long ThreadsCount = 2;
+        public static void SetPathsQueue(ICollection<string> imagesPaths)
+        {
+            if (imagesPaths == null) return;
+            lock (ImagesPaths)
+            {
+                ImagesPaths.Clear();
+                foreach (var a in imagesPaths) ImagesPaths.Enqueue(a);
+            }
+        }
+
+        public void Work()
+        {
+            var imagePath = "";
+            while (true)
+            {
+                lock (ImagesPaths)
+                {
+                    if (ImagesPaths.Count == 0) return;
+                    else imagePath = ImagesPaths.Dequeue();
+                }
+                ImagesEditor.EditImageFromFile(imagePath);
+            }
+        }
+    }
+}
