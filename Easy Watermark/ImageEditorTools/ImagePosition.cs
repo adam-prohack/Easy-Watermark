3e5bad68a403d00d1ef6b313bb5e9dc614486c88
Adam Brzozowski <adam_prohack@hotmail.com>
2017-05-23 03:04:13 +0200

    Added ImagePosition class

---
+++ Easy Watermark/ImageEditorTools/ImagePosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace EasyWatermark.ImagesEditorTools
+{
+    public class ImagePosition
+    {
+        public PointF FirstPoint { get; set; }
+        public PointF SecondPoint { get; set; }
+        public PointF ThirdPoint { get; set; }
+        public PointF[] GetArray()
+        {
+            return new PointF[]
+            {
+                FirstPoint,
+                SecondPoint,
+                ThirdPoint
+            };
+        }
+    }
+}
