b38d09223935ee7b8fa882305c5cab772c053583
Adam Brzozowski <adam_prohack@hotmail.com>
2017-05-23 03:07:30 +0200

    Added ImageResolution class

---
+++ Easy Watermark/ImageEditorTools/ImageResolution.cs
@@ -0,0 +1,18 @@
+﻿namespace EasyWatermark.ImagesEditorTools
+{
+    public class ImageResolution
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public ImageResolution()
+        {
+            Height = 0;
+            Width = 0;
+        }
+        public ImageResolution(float width, float height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
