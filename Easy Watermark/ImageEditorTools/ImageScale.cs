976d57a6106f72da5e96d581b0bf330dc5cede38
Adam Brzozowski <adam_prohack@hotmail.com>
2017-05-23 03:06:37 +0200

    Added ImageScale class

---
+++ Easy Watermark/ImageEditorTools/ImageScale.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EasyWatermark.ImagesEditorTools
+{
+    public enum ImageScaleDimension
+    {
+        Width, Height, Combined
+    }
+
+    public class ImageScale
+    {
+        public float Value { get; set; }
+        public ImageScaleDimension PropertyDimension { get; set; }
+
+        public ImageResolution GetComputedResolution(ImageResolution mainImageResolution, ImageResolution scaledImageResolution)
+        {
+            if (mainImageResolution == null || scaledImageResolution == null) throw new ArgumentNullException();
+            var result = new ImageResolution();
+            if (PropertyDimension == ImageScaleDimension.Combined)
+            {
+                result.Width = mainImageResolution.Width * Value;
+                result.Height = mainImageResolution.Height * Value;
+            }
+            else if (PropertyDimension == ImageScaleDimension.Width)
+            {
+                result.Width = mainImageResolution.Width * Value;
+                result.Height = scaledImageResolution.Height * mainImageResolution.Width * Value / scaledImageResolution.Width;
+            }
+            else if (PropertyDimension == ImageScaleDimension.Height)
+            {
+                result.Height = mainImageResolution.Height * Value;
+                result.Width = scaledImageResolution.Width * mainImageResolution.Height * Value / scaledImageResolution.Height;
+            }
+            else throw new NotImplementedException();
+            return result;
+        }
+
+        public ImageScale()
+        {
+            Value = 1;
+            PropertyDimension = ImageScaleDimension.Width;
+        }
+        public ImageScale(float scale, ImageScaleDimension propertyDimension)
+        {
+            Value = scale;
+            PropertyDimension = propertyDimension;
+        }
+    }
+}
