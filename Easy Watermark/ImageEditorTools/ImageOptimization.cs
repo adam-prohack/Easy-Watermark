0544c9f78e5f7ac691f655d835cb799aef42c3fb
Adam Brzozowski <adam_prohack@hotmail.com>
2017-05-23 03:04:02 +0200

    Added ImageOptimization class

---
+++ Easy Watermark/ImageEditorTools/ImageOptimization.cs
@@ -0,0 +1,18 @@
+﻿namespace EasyWatermark.ImagesEditorTools
+{
+    public class ImageOptimization
+    {
+        public ImageResolution MaximumResolution { get; set; }
+        public bool IsEnable { get; set; }
+        public ImageOptimization()
+        {
+            MaximumResolution = new ImageResolution(1920, 1080);
+            IsEnable = false;
+        }
+        public ImageOptimization(ImageResolution maximumResolution)
+        {
+            MaximumResolution = maximumResolution;
+            IsEnable = true;
+        }
+    }
+}
