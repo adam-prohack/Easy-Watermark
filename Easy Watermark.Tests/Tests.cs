a2f744baada7e644a7368c9c7861be36af77804e
Adam Brzozowski <adam_prohack@hotmail.com>
2017-06-11 01:20:50 +0200

    Added basic tests

---
+++ Easy Watermark.Tests/Tests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyWatermark;
+using System;
+
+namespace EasyWatermark.Tests
+{
+    [TestClass]
+    public class ImageEditorTests
+    {
+        string startDirectory = @"../../Resources/";
+        float marginScale = 0.05f;
+        float opacity = 0.5f;
+
+        [TestMethod]
+        public void CreateImagesSingleThread()
+        {
+            ImagesEditorWorker.ThreadsCount = 1;
+            ImagesEditor.Storage.ImagesDirectory = startDirectory + @"Images";
+            ImagesEditor.Storage.WatermarkPath = startDirectory + @"Watermark.png";
+            ImagesEditor.Watermark.Margin = new ImagesEditorTools.RelativeImageMargin(marginScale, marginScale, marginScale, marginScale);
+            ImagesEditor.Watermark.Opacity = opacity;
+            foreach (var a in Enum.GetValues(typeof(ImagesEditorTools.ImageAlignment)))
+            {
+                ImagesEditor.Watermark.Alignment = (ImagesEditorTools.ImageAlignment)a;
+                ImagesEditor.Storage.ResultImagesDirectory = startDirectory + @"Images/Results/SingleThread/" + Enum.GetName(typeof(ImagesEditorTools.ImageAlignment), a) + @"/";
+                ImagesEditor.EditImages();
+            }
+        }
+
+        [TestMethod]
+        public void CreateImagesMultithreads()
+        {
+            ImagesEditorWorker.ThreadsCount = 5;
+            ImagesEditor.Storage.ImagesDirectory = startDirectory + @"Images";
+            ImagesEditor.Storage.WatermarkPath = startDirectory + @"Watermark.png";
+            ImagesEditor.Watermark.Margin = new ImagesEditorTools.RelativeImageMargin(marginScale, marginScale, marginScale, marginScale);
+            ImagesEditor.Watermark.Opacity = opacity;
+            foreach (var a in Enum.GetValues(typeof(ImagesEditorTools.ImageAlignment)))
+            {
+                ImagesEditor.Watermark.Alignment = (ImagesEditorTools.ImageAlignment)a;
+                ImagesEditor.Storage.ResultImagesDirectory = startDirectory + @"Images/Results/MultiThreads/" + Enum.GetName(typeof(ImagesEditorTools.ImageAlignment), a) + @"/";
+                ImagesEditor.EditImages();
+            }
+        }
+    }
+}
